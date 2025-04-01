6ac212174e3867c2e2c042306890e807d3ce390e
abejoy <abrahamjoys98@gmail.com>
2025-04-01 10:32:30 +0100

    fixing more lint issues

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Contact from './components/Contact';
-import Testimonials from './components/Testimonials';
-import resumeData from './assets/resumeData.json'
-import { ReactNotifications } from 'react-notifications-component'
-import 'react-notifications-component/dist/theme.css'
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import Testimonials from "./components/Testimonials";
+import resumeData from "./assets/resumeData.json";
+import { ReactNotifications } from "react-notifications-component";
+import "react-notifications-component/dist/theme.css";
 
-const App = () =>  {
+const App = () => {
   return (
-      <div className="App">
-        <ReactNotifications />
-        <Header data={resumeData.main}/>
-        <Testimonials data={resumeData.testimonials}/>
-        <Contact data={resumeData.main}/>
-        <Footer data={resumeData.main}/>
-      </div>
-  )}
+    <div className="App">
+      <ReactNotifications />
+      <Header data={resumeData.main} />
+      <Testimonials data={resumeData.testimonials} />
+      <Contact data={resumeData.main} />
+      <Footer data={resumeData.main} />
+    </div>
+  );
+};
 export default App;
