47b671258cff54648af9af85fa7b81297d72d971
abejoy <abrahamjoys98@gmail.com>
2025-04-10 21:41:27 +0100

    fixing linting

--- src/components/FootballStats.js
+++ src/components/FootballStats.js
@@ -6,8 +6,8 @@ const FootballStats = (props) => {
   return (
     <>
         <BarNav />
-        <div className="app" style={{backgroundColor: '#1F1F1F'}} >
-        <iframe style={{width:'100%', height: '85vh' }} src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTeQ2089RDdIWG3hRSahC_rYrUF6dgSb-GJCMVYQEPD4heBUJdif-aSc_a_cC9G0VdblQN1wKlkat5c/pubhtml?gid=878056535&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+        <div style={{backgroundColor: '#1F1F1F'}} >
+        <iframe title="quarterfinals" style={{width:'100%', height: '85vh' }} src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTeQ2089RDdIWG3hRSahC_rYrUF6dgSb-GJCMVYQEPD4heBUJdif-aSc_a_cC9G0VdblQN1wKlkat5c/pubhtml?gid=878056535&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
         </div>
   </>
   );
